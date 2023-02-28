9b9fe8cf01ec0e6e23ac5000b13b62e3f4a72426
bimm1010 <thanhnamhoang1010@gmail.com>
2023-02-28 10:34:26 +0700

    coming

---
+++ lib/pages/loginScreen/welcome_login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomeLoginScreen extends StatelessWidget {
+  const WelcomeLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('login screen');
+  }
+}
