cb0c8d3082d209fbc25ee5428a85287c511a26ce
bimm1010 <thanhnamhoang1010@gmail.com>
2023-05-08 11:11:51 +0700

    continue

--- lib/pages/restaurants/list_featured_items.dart
+++ lib/pages/restaurants/list_featured_items.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:tamang_food_service/theme/theme.dart';
 
 class ListFeaturedItems extends StatelessWidget {
   const ListFeaturedItems({
     super.key,
+    required this.image,
+    required this.name,
+    required this.location,
   });
+  final String image;
+  final String name;
+  final String location;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          width: 200,
-          child: Column(
-            children: [
-              AspectRatio(
-                aspectRatio: 1.25,
-                child: Image.asset('assets/images/f_0.png'),
-              ),
-            ],
+    return SizedBox(
+      width: 200,
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1.20,
+            child: Image.asset(image),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: defaultPadding, vertical: defaultPadding),
+              child: Text(
+                name,
+                style: PrimaryFont.light(16),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: Row(
+              children: [
+                const Text('\$\$'),
+                const Padding(
+                  padding: EdgeInsets.only(
+                      left: defaultPadding, right: defaultPadding - 10),
+                  child: CircleAvatar(
+                    backgroundColor: kColorTextField,
+                    radius: 2.0,
+                  ),
+                ),
+                Text(location)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/restaurants/main_restaurants.dart
+++ lib/pages/restaurants/main_restaurants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tamang_food_service/pages/restaurants/restaurants_info.dart';
+import 'package:tamang_food_service/theme/list_data.dart';
 import 'package:tamang_food_service/theme/theme.dart';
 import 'list_featured_items.dart';
 import 'restaurants_app_bar.dart';
@@ -17,26 +18,36 @@ class MainRestaurants extends StatelessWidget {
             child: RestaurantsInfo(),
           ),
           SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: defaultPadding * 2.0,
-                      left: defaultPadding,
-                      right: defaultPadding,
-                      bottom: defaultPadding,
-                    ),
-                    child: Text(
-                      'Featured Items',
-                      style: PrimaryFont.light(20),
-                    ),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: defaultPadding * 2.0,
+                  left: defaultPadding,
+                  right: defaultPadding,
+                  bottom: defaultPadding,
+                ),
+                child: Text(
+                  'Featured Items',
+                  style: PrimaryFont.light(20),
+                ),
+              ),
+            ),
+            // list image
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  listFeaturedItems.length,
+                  (index) => ListFeaturedItems(
+                    image: listFeaturedItems[index]['image'],
+                    name: listFeaturedItems[index]['name'],
+                    location: listFeaturedItems[index]['location'],
                   ),
                 ),
-                // list image
-                const ListFeaturedItems(),
-              ],
+              ),
             ),
           ),
         ],
